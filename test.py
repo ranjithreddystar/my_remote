d17fd648f6e2d86d1ce06a7c8224551bd263af7c
ranjith <ranjithkumarpenthala@gmail.com>
2023-04-24 10:28:25 +0530

    second commit

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> for i in range(10):
-...     print('Ranjith')
+
+for i in range(10):
+    print('Ranjith')
+print('#'*50)
